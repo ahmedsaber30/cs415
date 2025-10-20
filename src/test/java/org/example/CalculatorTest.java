13cd4a00a90e1b8400e02519a944a49a8bc067c7
ahmedsaber30 <146309815+ahmedsaber30@users.noreply.github.com>
2025-10-20 21:36:35 +0300

    wrong push test should fail

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
     @Test
     void testAddition() {
         Calculator calculator = new Calculator(); // Create instance
-        int result = calculator.add(2, 3);        // Call the function
+        int result = calculator.add(2 , 3);        // Call the function
         assertEquals(6, result);                  // Check result
     }
 }
